eb52c98254010dac337bfc462ae436ec1fff8791
itsanshul96 <me.anshul96@gmail.com>
2021-06-14 16:51:42 +0530

    added hbs files in to the app.js so that frontend user can do the work on UI

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hi I am Anshul</h1>");
+app.set('view engine', 'hbs');
+
+app.get("/home", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
 });
 
 app.listen(5000, () => {
