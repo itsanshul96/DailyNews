492bbbae8f503d52099aa305af3b95d87009034c
itsanshul96 <me.anshul96@gmail.com>
2021-06-20 21:39:41 +0530

    Developed the backend of user registration in daily news

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
 const app = express();
-
+const mysql = require("mysql");
 const dotenv = require("dotenv");
+require('dotenv').config();
+
+dotenv.config({path:'/.env'});
 
 dotenv.config({path: './.env'});
 
@@ -12,26 +14,36 @@ const {
 
 const path = require("path");
 
+const { rootCertificates } = require('tls');
+
 const publicDirectory = path.join(__dirname, './public');
 
 app.use(express.static(publicDirectory));
 
+//Parse url-encodded bodies (as sent by html forms)
+
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(express.json());
 
 app.set('view engine', 'hbs');
 
-app.get("/home", (req, res) => {
-    res.render("home");
+//Define routes
+app.use('/',require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+
+db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+
 });
 
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-app.get("/register", (req, res) => {
-    res.render("register");
+db.connect((error) =>{
+    error ? console.log('There is ' + error) : console.log('Mysql Connected');
 });
 
 app.listen(5000, () => {
---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+
+});
+
+exports.register = (req, res) => {
+    console.log(req.body);
+    const {
+        name,
+        email,
+        password
+    } = req.body;
+
+    db.query("select email from daily_news_users where email= ?", [email], async (error, results) => {
+        if (error) {
+            console.log('There is an error in your connection which is ' + error);
+        } else {
+            if (results.length > 0) {
+                return res.render('register', {
+                    message: 'This Email id is already registerd with us.'
+                });
+            }
+        }
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        db.query('insert into daily_news_users set ?', {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }, (error, results) => {
+            if (error) {
+                console.log('Error in saving Data ' + error);
+            } else {
+                console.log('I am printing results ' + results);
+                return res.render('home', {
+                    message: "You are succesfully registerd with DailyNews, Let's read something meaningful"
+                });
+            }
+        });
+    });
+}
